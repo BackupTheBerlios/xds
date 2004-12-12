c4ed846b09abd5a9e5281ad7c1e5eefbed54fc16
vsoft <vsoft>
2004-12-12 21:46:38 +0000

    Testovací změna

--- src/cz/xds/command/pwd.java
+++ src/cz/xds/command/pwd.java
@@ -7,9 +7,11 @@ import cz.xds.*;
  * User: Vašek
  * Date: 12.12.2004
  * Time: 21:45:47
+ *
+ * zkouška pøidání øádku
  */
 public class pwd implements Command {
 	public void execute(Object[] param) {
         System.out.println(((FileSystem)param[0]).getCurrentDirectory().getFullPath());
-}
+    }
 }
